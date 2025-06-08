aec34ffa4b5ffc17186aeeaf909e7e2bbecbe88b
gimnala <gimnala613@gmail.com>
2025-06-08 23:42:04 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from flask import Flask, render_template, request, redirect, url_for, session, abort, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 import datetime
 
 app = Flask(__name__)
@@ -71,7 +72,11 @@ def index():
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
     if request.method == 'POST':
-        user = User(username=request.form['username'], password=request.form['password'])
+        if User.query.filter_by(username=request.form['username']).first():
+            flash('이미 존재하는 사용자 이름입니다.')
+            return redirect('/signup')
+        hashed_pw = generate_password_hash(request.form['password'])
+        user = User(username=request.form['username'], password=hashed_pw)
         db.session.add(user)
         db.session.commit()
         return redirect('/login')
@@ -80,10 +85,11 @@ def signup():
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        user = User.query.filter_by(username=request.form['username'], password=request.form['password']).first()
-        if user:
+        user = User.query.filter_by(username=request.form['username']).first()
+        if user and check_password_hash(user.password, request.form['password']):
             login_user(user)
             return redirect('/')
+        flash('로그인 실패')
     return render_template('login.html')
 
 @app.route('/logout')
@@ -103,147 +109,14 @@ def add_book():
         return redirect('/search')
     return render_template('add_book.html')
 
-@app.route('/book/<int:book_id>')
-@login_required
-def book_detail(book_id):
-    book = Book.query.get_or_404(book_id)
-    groupbuy = GroupBuy.query.filter_by(book_id=book_id).first()
-    participants = []
-    joined = False
-
-    if groupbuy:
-        joined = GroupBuyParticipant.query.filter_by(user_id=current_user.id, groupbuy_id=groupbuy.id).first() is not None
-        for p in GroupBuyParticipant.query.filter_by(groupbuy_id=groupbuy.id):
-            user = User.query.get(p.user_id)
-            if user:
-                participants.append(user.username)
-
-    creator_user = User.query.get(book.creator_id) if book.creator_id else None
-    return render_template('book_detail.html', book=book, groupbuy=groupbuy, joined=joined, participants=participants, creator_user=creator_user)
-
-@app.route('/groupbuy/start/<int:book_id>', methods=['POST'])
-@login_required
-def start_groupbuy(book_id):
-    start = datetime.datetime.now()
-    end = start + datetime.timedelta(days=7)
-    groupbuy = GroupBuy(book_id=book_id, starter_id=current_user.id, start_date=start, end_date=end)
-    db.session.add(groupbuy)
-    db.session.commit()
-    return redirect(f'/book/{book_id}')
-
-@app.route('/groupbuy/join/<int:groupbuy_id>', methods=['POST'])
-@login_required
-def join_groupbuy(groupbuy_id):
-    if not GroupBuyParticipant.query.filter_by(user_id=current_user.id, groupbuy_id=groupbuy_id).first():
-        db.session.add(GroupBuyParticipant(user_id=current_user.id, groupbuy_id=groupbuy_id))
-        db.session.commit()
-    gb = GroupBuy.query.get(groupbuy_id)
-    return redirect(f'/book/{gb.book_id}')
-
-@app.route('/groupbuy/cancel/<int:groupbuy_id>', methods=['POST'])
-@login_required
-def cancel_groupbuy(groupbuy_id):
-    gb = GroupBuy.query.get_or_404(groupbuy_id)
-    if gb.starter_id != current_user.id:
-        abort(403)
-    db.session.delete(gb)
-    db.session.commit()
-    return redirect(f'/book/{gb.book_id}')
-
-@app.route('/cancel_participation/<int:groupbuy_id>', methods=['GET'])
-@login_required
-def cancel_participation(groupbuy_id):
-    participation = GroupBuyParticipant.query.filter_by(groupbuy_id=groupbuy_id, user_id=current_user.id).first()
-    if participation:
-        db.session.delete(participation)
-        db.session.commit()
-        flash('공동구매 참여가 취소되었습니다.')
-    else:
-        flash('취소할 수 있는 참여 정보가 없습니다.')
-    return redirect('/mypage')
-
-@app.route('/book/delete/<int:book_id>', methods=['POST'])
-@login_required
-def delete_book(book_id):
-    book = Book.query.get_or_404(book_id)
-    if book.creator_id != current_user.id:
-        abort(403)
-    db.session.delete(book)
-    db.session.commit()
-    return redirect('/search')
-
-@app.route('/delete_account', methods=['POST'])
-@login_required
-def delete_account():
-    user_id = current_user.id
-    logout_user()
-    GroupBuyParticipant.query.filter_by(user_id=user_id).delete()
-    Comment.query.filter_by(author_id=user_id).delete()
-    Post.query.filter_by(user_id=user_id).delete()
-    user_to_delete = User.query.get(user_id)
-    db.session.delete(user_to_delete)
-    db.session.commit()
-    return redirect('/')
-
 @app.route('/search')
 def search():
     q = request.args.get('q', '')
     books = Book.query.filter(Book.title.contains(q)).all() if q else []
     return render_template('search_results.html', books=books)
 
-@app.route('/notice', methods=['GET', 'POST'])
-@login_required
-def notice():
-    if request.method == 'POST':
-        n = Notice(title=request.form['title'], content=request.form['content'], author_id=current_user.id)
-        db.session.add(n)
-        db.session.commit()
-        return redirect('/notice')
-    notices = Notice.query.all()
-    return render_template('notice.html', notices=notices)
-
-@app.route('/notice/<int:notice_id>', methods=['GET', 'POST'])
-@login_required
-def notice_detail(notice_id):
-    notice = Notice.query.get_or_404(notice_id)
-    if request.method == 'POST':
-        comment = Comment(content=request.form['content'], notice_id=notice_id, author_id=current_user.id)
-        db.session.add(comment)
-        db.session.commit()
-    comments = Comment.query.filter_by(notice_id=notice_id).all()
-    return render_template('notice_detail.html', notice=notice, comments=comments)
-
-@app.route('/notice/delete/<int:notice_id>', methods=['POST'])
-@login_required
-def delete_notice(notice_id):
-    n = Notice.query.get_or_404(notice_id)
-    if n.author_id != current_user.id:
-        abort(403)
-    db.session.delete(n)
-    db.session.commit()
-    return redirect('/notice')
-
-@app.route('/comment/delete/<int:comment_id>', methods=['POST'])
-@login_required
-def delete_comment(comment_id):
-    c = Comment.query.get_or_404(comment_id)
-    nid = c.notice_id
-    if c.author_id != current_user.id:
-        abort(403)
-    db.session.delete(c)
-    db.session.commit()
-    return redirect(f'/notice/{nid}')
-
-@app.route('/mypage')
-@login_required
-def mypage():
-    mybooks = Book.query.filter_by(creator_id=current_user.id).all()
-    mygroupbuys = GroupBuyParticipant.query.filter_by(user_id=current_user.id).all()
-    gb_ids = [g.groupbuy_id for g in mygroupbuys]
-    joined_books = GroupBuy.query.filter(GroupBuy.id.in_(gb_ids)).all()
-    return render_template('mypage.html', books=mybooks, joined_ids=gb_ids, joined_books=joined_books)
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    with app.app_context():
-        db.create_all()
     app.run(debug=True)
